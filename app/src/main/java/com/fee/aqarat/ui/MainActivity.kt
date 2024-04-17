64dd02b6fc07fd3c010393e5ccc2d908a3251e59
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2024-04-17 07:49:24 +0200

    initial commit: setUp Dependencies

---
+++ app/src/main/java/com/fee/aqarat/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fee.aqarat.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fee.aqarat.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
