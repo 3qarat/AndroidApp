7ad91a1406e7e837a4186240b655da12e981060e
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2024-06-26 21:31:47 +0300

    auth: navigation ⭐⭐

--- app/src/main/java/com/fee/aqarat/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/fee/aqarat/ui/auth/login/LoginFragment.kt
@@ -3,6 +3,7 @@ package com.fee.aqarat.ui.auth.login
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.fee.aqarat.base.BaseFragment
 import com.fee.aqarat.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +18,20 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
 
     override fun prepareView(savedInstanceState: Bundle?) {
 
+        initUi()
+
+    }
+
+    private fun initUi() {
+        binding.forgetPasswordTextView.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToForgetPasswordFragment()
+            findNavController().navigate(action)
+        }
+
+        binding.createAccountTextView.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+            findNavController().navigate(action)
+        }
     }
 
 
